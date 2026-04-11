1c6e7b53bbd632ba53ee64ff89ca0544c72259b9
skasti <stig-rune.skansgard@outlook.com>
2026-04-11 22:08:38 +0200

    Remove the placeholders

--- genome/src/main/kotlin/life/sim/genome/GenomeHello.kt
+++
@@ -1,6 +0,0 @@
-package life.sim.genome
-
-/**
- * Simple placeholder demonstrating that the genome module can build independently.
- */
-fun genomeGreeting(): String = "Hello from the genome module!"
